988b72ba65a7106336a966b5921f020ba7e825c3
Yuki <risaton@risaton.net>
2022-12-26 09:30:28 +0900

    updated some files

---
+++ forge.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  packagerConfig: {
+    // osxSign:{
+    //   // optionsForFile: (filePath) =>{
+    //   //   return{
+    //   //     entitlements: ''
+    //   //   }
+    //   // }
+    // }
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
--- index.js
+++ index.js
@@ -28,3 +28,4 @@ app.whenReady().then(() => {
         }
     })
 })
+//aaa
